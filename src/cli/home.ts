81b42fbdb9f346afb9d63c0838352ecca0da1d44
Fabian Darga <kontakt@fabiandarga.de>
2024-12-15 23:18:08 +0100

    Step 1

--- src/cli/home.ts
+++ src/cli/home.ts
@@ -1,1 +1,21 @@
-class TerminalHome {}
+import Terminal from "./terminal";
+
+export default class TerminalHome extends Terminal {
+  private username;
+
+  constructor(username: string) {
+    super();
+    this.username = username;
+  }
+
+  printWelcomeMessage() {
+    this.print([
+      `Guten Tag ${this.username}!`,
+      "",
+      "Heute ist ein toller Tag zum Programmieren.",
+      "",
+      "Hier ist dein Terminal Home...",
+    ]);
+    this.printLine();
+  }
+}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,4 @@
-const radioUrl = 'http://stream.jam.fm/jamfm-bl/mp3-192/';
+import TerminalHome from './cli/home';
 
-import Player from 'play-sound';
-
-const radioPlayer = Player({
-  player: 'mplayer',
-});
-
-// $ mplayer foo.mp3
-const audio = radioPlayer.play(radioUrl, function (err) {
-  if (err) throw err;
-});
-
-setTimeout(() => {
-  console.log('killing audio');
-  audio.kill();
-}, 3000);
+const terminal = new TerminalHome(process.env.USER || 'Nobody');
+terminal.printWelcomeMessage();
